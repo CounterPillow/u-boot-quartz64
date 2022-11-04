e77f19caae2811c96ac56ded8fe9d9a2837883df
Nicolas Frattaroli <frattaroli.nicolas@gmail.com>
2022-11-04 18:33:07 +0100

    rockchip: rk356x: Move compressed kernel address
    Thicc kernels need more space than I previously provided for,
    and it would clobber the FDT otherwise.
    
    Signed-off-by: Nicolas Frattaroli <frattaroli.nicolas@gmail.com>

--- include/configs/rk3568_common.h
+++ include/configs/rk3568_common.h
@@ -39,7 +39,7 @@
 	"fdt_addr_r=0x0a100000\0"	\
 	"kernel_addr_r=0x02080000\0"	\
 	"ramdisk_addr_r=0x0a200000\0" \
-	"kernel_comp_addr_r=0x08000000\0" \
+	"kernel_comp_addr_r=0x0f000000\0" \
 	"kernel_comp_size=0x2000000\0"
 
 #include <config_distro_bootcmd.h>
